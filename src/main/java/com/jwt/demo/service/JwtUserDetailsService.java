f164e7dd78f32f5939ace4006bc3cd3162fc2e75
cherraniram@gmail.com <cherraniram@gmail.com>
2023-01-30 22:18:16 +0300

    -------

--- src/main/java/com/jwt/demo/service/JwtUserDetailsService.java
+++ src/main/java/com/jwt/demo/service/JwtUserDetailsService.java
@@ -1,23 +1,27 @@
 package com.jwt.demo.service;
 
+
+
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import org.springframework.security.core.userdetails.UserDetailsService;
-
 import java.util.ArrayList;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
 @Service
-public class JwtUserDetailsService {
-//    @Override
+public class JwtUserDetailsService implements  UserDetailsService {
+    @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         if ("javainuse".equals(username)) {
-            return new User("supersecretpassword", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+            return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
                     new ArrayList<>());
         } else {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
     }
 }
+
